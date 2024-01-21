e70ca689fa7735d098ac4267085bde4484f12962
Maria Hochstoeger <m_hochstoeger@hotmail.com>
2024-01-21 13:37:35 +0000

    Display only posts with published status in the view

--- blog/views.py
+++ blog/views.py
@@ -5,5 +5,5 @@ from .models import Post # import Post model
 # VIEWS
 # CLASS-BASED VIEWS
 class PostList(generic.ListView):
-    queryset = Post.objects.all()
-    template_name = "post_list.html"
+    queryset = Post.objects.filter(status=1) # returns all records from the Post model with status 1, ie published
+    template_name = "post_list.html" # post_list: post refers to the model, list to the generic view
