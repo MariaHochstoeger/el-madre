38ba9db0669a02f7af1615fcaf7a0bc80ed7e009
Maria Hochstoeger <m_hochstoeger@hotmail.com>
2024-01-21 10:48:52 +0000

    Migrate Comment model and add to admin panel

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
 
 # Register your models here so that they appear on the admin site
 admin.site.register(Post)
+admin.site.register(Comment)
