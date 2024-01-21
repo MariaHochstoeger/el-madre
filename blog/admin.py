5596a1442cebe962f74e7ee99ac633df15983c31
Maria Hochstoeger <m_hochstoeger@hotmail.com>
2024-01-21 14:19:23 +0000

    Add functionality for superuser to publish several draft posts at once

--- blog/admin.py
+++ blog/admin.py
@@ -6,12 +6,18 @@ from django_summernote.admin import SummernoteModelAdmin # extra functionality f
 @admin.register(Post) # decorator to register the class Post
 class PostAdmin(SummernoteModelAdmin):
 
-    list_display = ('title', 'slug', 'status')
-    search_fields = ['title']
-    list_filter = ('status',)
+    list_display = ('title', 'slug', 'author', 'status', 'created_on') # added 'author' for better UX
+    search_fields = ['title', 'content']
+    list_filter = ('status', 'created_on', 'author') # added 'author' for better UX
     prepopulated_fields = {'slug': ('title',)}
     summernote_fields = ('content',)
 
+    # Custom code: publish several posts at once
+    actions = ['make_published']
+
+    def make_published(self, request, queryset):
+        queryset.update(status=1)
+    make_published.short_description = "Mark selected posts as published"
 
 # Registering models so that they appear on the admin site
 admin.site.register(Comment)
