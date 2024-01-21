b28adee980f353fbf51ed16d7735fcc93f832a29
Maria Hochstoeger <m_hochstoeger@hotmail.com>
2024-01-21 10:43:35 +0000

    Add Comment model

--- elmadre/settings.py
+++ elmadre/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-mariahochstoege-elmadre-2ehblpit216.ws-eu107.gitpod.io','.herokuapp.com']
 
