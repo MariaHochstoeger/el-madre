47b4e51d4f2d616f6d01897a448dd5cecfd44258
Maria Hochstoeger <m_hochstoeger@hotmail.com>
2024-01-22 10:12:39 +0000

    Add post_detail.html and default image

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import generic # import generic django view
 from .models import Post # import Post model
 
@@ -7,3 +7,27 @@ class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1) # returns all records from the Post model with status 1, ie published
     template_name = "blog/index.html"
     paginate_by = 3
+
+
+def post_detail(request, slug):
+    """
+    Display an individual :model:`blog.Post`.
+
+    **Context**
+
+    ``post``
+        An instance of :model:`blog.Post`.
+
+    **Template:**
+
+    :template:`blog/post_detail.html`
+    """
+
+    queryset = Post.objects.filter(status=1)
+    post = get_object_or_404(queryset, slug=slug)
+
+    return render(
+        request,
+        "blog/post_detail.html",
+        {"post": post},
+    )
