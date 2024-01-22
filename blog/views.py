6b36dda41f9c529408b4ae2828267e3ef9a578db
Maria Hochstoeger <m_hochstoeger@hotmail.com>
2024-01-22 11:00:52 +0000

    Remove tags in post previews by using striptags

--- blog/views.py
+++ blog/views.py
@@ -3,12 +3,15 @@ from django.views import generic # import generic django view
 from .models import Post # import Post model
 
 # VIEWS
+
+# Class-based views
 class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1) # returns all records from the Post model with status 1, ie published
     template_name = "blog/index.html"
     paginate_by = 3
 
 
+# Function-based views
 def post_detail(request, slug):
     """
     Display an individual :model:`blog.Post`.
