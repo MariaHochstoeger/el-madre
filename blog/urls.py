00becb4b8644069c6224b10bf9b0a3069177b660
Maria Hochstoeger <m_hochstoeger@hotmail.com>
2024-01-22 10:20:26 +0000

    Save all files

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,5 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'), # blank space '' signifies home page
+    path('<slug:slug>/', views.post_detail, name='post_detail'),
 ]
