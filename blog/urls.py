e4c84523342b58daee636ebae3af09486d786803
Maria Hochstoeger <m_hochstoeger@hotmail.com>
2024-01-23 17:13:36 +0000

    Safety commit

--- blog/urls.py
+++ blog/urls.py
@@ -5,9 +5,9 @@ from . import views # import views file
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'), # blank space '' signifies home page
     path('<slug:slug>/', views.post_detail, name='post_detail'),
+    path('category/<category>/', views.CatListView.as_view(), name='category'),
     path('<slug:slug>/edit_comment/<int:comment_id>',
          views.comment_edit, name='comment_edit'),
     path('<slug:slug>/delete_comment/<int:comment_id>',
          views.comment_delete, name='comment_delete'),
-    path('category/<category>/', views.CatListView.as_view(), name='category'),
 ]
